55ff6de14facc12ce6585cf4298b956f47f84a14
atavtorkin <tavtorkin@jivosite.com>
2021-06-11 16:48:22 +0300

    Add example setting custom UI

--- app/src/main/java/com/jivosite/jivosdk_android_sample/App.kt
+++ app/src/main/java/com/jivosite/jivosdk_android_sample/App.kt
@@ -2,6 +2,7 @@ package com.jivosite.jivosdk_android_sample
 
 import android.app.Application
 import com.jivosite.sdk.Jivo
+import com.jivosite.sdk.support.builders.Config
 
 /**
  * Created on 4/19/21.
@@ -15,6 +16,19 @@ class App : Application() {
         Jivo.init(
             appContext = this,
             widgetId = "Q7BcPYNqCG"
+        )
+        Jivo.setConfig(
+            Config.Builder()
+                .setLogo(R.drawable.vic_jivosdk_logo)
+                .setBackground(R.drawable.bg_jivosdk_appbar)
+                .setTitle(R.string.jivosdk_title)
+                .setTitleTextColor(R.color.white)
+                .setSubtitle(R.string.jivosdk_subtitle)
+                .setSubtitleTextColor(R.color.white)
+                .setSubtitleTextColorAlpha(0.6f)
+                .setWelcomeMessage(R.string.jivosdk_welcome)
+                .setOutgoingMessageColor(Config.Color.GREY)
+                .build()
         )
     }
 }
